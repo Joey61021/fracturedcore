41743b15d2a754a048059b6335a8590644361344
Cory Torode <cowtorode@gmail.com>
2025-01-23 11:04:46 -0500

    Removes the player from the team's online members list

--- src/main/java/com/fractured/events/LeaveListener.java
+++ src/main/java/com/fractured/events/LeaveListener.java
@@ -3,11 +3,13 @@ package com.fractured.events;
 import com.fractured.FracturedCore;
 import com.fractured.team.Team;
 import com.fractured.user.User;
-import com.fractured.util.Utils;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import static com.fractured.util.Utils.color;
 
 public class LeaveListener implements Listener
 {
@@ -18,6 +20,13 @@ public class LeaveListener implements Listener
         User user = FracturedCore.getUserManager().getUser(player.getUniqueId());
         Team team = user.getTeam();
 
-        event.setQuitMessage(Utils.color((team != null ? team.color() : "&7") + player.getName() + " &fhas disconnected"));
+        if (team == null)
+        {
+            event.setQuitMessage(color("&7" + player.getName() + " &fhas disconnected"));
+        } else
+        {
+            team.getOnlineMembers().remove(player);
+            event.setQuitMessage(team.color() + player.getName() + ChatColor.WHITE + " has disconnected");
+        }
     }
 }
