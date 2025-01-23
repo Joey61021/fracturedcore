b8cf53e9c574b65d5e59cc3fd4f062032cabc95b
Cory Torode <cowtorode@gmail.com>
2025-01-23 11:05:39 -0500

    Changed comment

---
+++ src/main/java/com/fractured/user/User.java
@@ -0,0 +1,65 @@
+package com.fractured.user;
+
+import com.fractured.team.Team;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
+
+public class User
+{
+    private final UUID uuid;
+    /**
+     * As in the database
+     */
+    private int userId;
+    private boolean inTeamChat;
+    private Team team;
+
+    public User(UUID uuid)
+    {
+        this.uuid = uuid;
+    }
+
+    public UUID getUuid()
+    {
+        return uuid;
+    }
+
+    public void setUserId(int userId)
+    {
+        this.userId = userId;
+    }
+
+    public int getUserId()
+    {
+        return userId;
+    }
+
+    public boolean isInTeamChat()
+    {
+        return inTeamChat;
+    }
+
+    public void setInTeamChat(boolean inTeamChat)
+    {
+        this.inTeamChat = inTeamChat;
+    }
+
+    public Team getTeam()
+    {
+        return team;
+    }
+
+    /**
+     * Use {@link com.fractured.team.TeamManager#addTeam(Player, Team)} or {@link com.fractured.team.TeamManager#removeTeam(Player)}
+     */
+    public void setTeam(Team team)
+    {
+        this.team = team;
+        // This makes it so that you have to retoggle team chat if you join a new team.
+        // This also guarentees that you can't be in teamchat if you don't have a team,
+        // (coupled with a check that checks before you set in team chat if you are in
+        // a team)
+        inTeamChat = false;
+    }
+}
