4afd0a16a7a84c25dafa1e8e46a79c7e477fabe7
Cory Torode <cowtorode@gmail.com>
2025-01-23 11:07:10 -0500

    Fixed incorrect initialization.

--- src/main/java/com/fractured/team/TeamCache.java
+++ src/main/java/com/fractured/team/TeamCache.java
@@ -7,10 +7,7 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public final class TeamCache
 {
@@ -20,20 +17,38 @@ public final class TeamCache
     }
 
     private static Inventory teamInventory;
-    private static Map<Integer, Team> teams;
+    private static Map<Integer, Team> teamsById;
+    /**
+     * Used for commands like /team set [username] [name]
+     */
     private static Map<String, Team> teamsByName;
 
-    public static void init(Map<Integer, Team> teams)
+    public static void init(Map<Integer, Team> teamsIdMap)
     {
-        TeamCache.teams = teams;
+        TeamCache.teamsById = teamsIdMap;
+        teamsByName = new HashMap<>();
 
+        // init team inventory (and teamsByName)
         teamInventory = Bukkit.createInventory(null, 3 * 9, Utils.color("&7Select Team"));
+        int i = 0;
         ItemStack item;
         ItemMeta meta;
 
-        for (int i = 0; i < 4; ++i)
+        // todo Right now we only need to consider the first four teams
+        // This could be changed if we wanted to add more, but there's
+        // no way to add teams right now, so there's no point in porting
+        // this.
+        for (Team team : teamsIdMap.values())
         {
-            Team team = teams.get(i);
+            // fixme This probably isn't the best way to do this, I
+            // just did it like this because it seemed the simplest
+
+            // Add the teamsByName mapping START
+
+            teamsByName.put(team.getName().toLowerCase(), team);
+
+            // END
+
             item = new ItemStack(team.material());
             meta = item.getItemMeta();
 
@@ -49,18 +64,23 @@ public final class TeamCache
                 item.setItemMeta(meta);
             }
 
-            teamInventory.setItem(9 + 1, item);
+            teamInventory.setItem(9 + 1 + 2 * (i++), item);
         }
     }
 
     public static Collection<Team> teams()
     {
-        return teams.values();
+        return teamsById.values();
     }
 
     public static Team getTeam(int id)
     {
-        return teams.get(id);
+        return teamsById.get(id);
+    }
+
+    public static Team getTeam(String name)
+    {
+        return teamsByName.get(name.toLowerCase());
     }
 
     public static void openMenu(Player player)
