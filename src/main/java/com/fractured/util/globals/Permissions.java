986ed75f8256e6c702a7d881c3240d441409a12d
Cory Torode <cowtorode@gmail.com>
2025-02-02 15:01:43 -0500

    Removed removed permissions

--- src/main/java/com/fractured/util/globals/Permissions.java
+++ src/main/java/com/fractured/util/globals/Permissions.java
@@ -3,12 +3,9 @@ package com.fractured.util.globals;
 public final class Permissions
 {
     public static final String COMMAND_TEAM_ADMIN = "fractured.command.team.admin";
-    public static final String COMMAND_REGION_WAND = "fractured.command.region_wand";
     public static final String COMMAND_SETTINGS_ADMIN = "fractured.command.settings.admin";
     public static final String COMMAND_BORDERS_ADMIN = "fractured.command.generate.admin";
-    public static final String COMMAND_WORLD_LIST = "fractured.command.world.list";
-    public static final String COMMAND_WORLD_GENERATE = "fractured.command.world.generate";
-    public static final String COMMAND_WORLD_TELEPORT = "fractured.command.world.teleport";
+    public static final String COMMAND_WORLD_ADMIN = "fractured.command.world.*";
 
     private Permissions()
     {
