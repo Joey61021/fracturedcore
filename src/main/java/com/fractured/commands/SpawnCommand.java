5db6936f03a1a06adc78a6ecfcdeada3f1b27dfa
Joey61021 <joseph.gardiner58@gmail.com>
2025-02-20 16:46:00 +1000

    Fix spawn command

--- src/main/java/com/fractured/commands/SpawnCommand.java
+++ src/main/java/com/fractured/commands/SpawnCommand.java
@@ -1,7 +1,7 @@
 package com.fractured.commands;
 
 import com.fractured.FracturedCore;
-import com.fractured.user.User;
+import com.fractured.team.Team;
 import com.fractured.user.UserManager;
 import com.fractured.util.globals.Messages;
 import org.bukkit.command.Command;
@@ -12,22 +12,22 @@ public final class SpawnCommand
 {
     public static boolean spawn(final CommandSender sender, final Command command, String label, final String[] args)
     {
-        if (!(sender instanceof Player))
+        if (!(sender instanceof Player player))
         {
             sender.sendMessage(FracturedCore.getMessages().get(Messages.COMMAND_CONSOLE_BLOCKED));
             return true;
         }
 
-        User user = UserManager.getUser((Player) sender);
+        Team team = UserManager.getUser(player).getTeam();
 
-        if (user.getTeam() == null)
+        if (team == null)
         {
-            sender.sendMessage(FracturedCore.getMessages().get(Messages.CMD_SPAWN_NOT_IN_REGION));
+            player.sendMessage(FracturedCore.getMessages().get(Messages.CMD_SPAWN_NOT_IN_REGION));
             return true;
         }
 
-        ((Player) sender).teleport(user.getTeam().spawn());
-        sender.sendMessage(FracturedCore.getMessages().get(Messages.CMD_SPAWN_TELEPORTED));
+        player.teleport(team.spawn());
+        sender.sendMessage(FracturedCore.getMessages().get(Messages.CMD_SPAWN_TELEPORTED).replace("%team%", team.color() + team.getName()));
         return true;
     }
 
