2cedbf5fb9a7486cb4308b6a00228d483148235e
Cory Torode <cowtorode@gmail.com>
2025-03-26 10:08:48 -0400

    Added Click Event

--- src/main/java/com/fractured/cevents/pantheon/Prompt.java
+++ src/main/java/com/fractured/cevents/pantheon/Prompt.java
@@ -1,7 +1,6 @@
 package com.fractured.cevents.pantheon;
 
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.event.ClickEvent;
 
 public final class Prompt
 {
@@ -17,11 +16,6 @@ public final class Prompt
     public Prompt(String prompt, Response response)
     {
         this(Component.text(prompt), response);
-    }
-
-    protected void setClickEvent(ClickEvent event)
-    {
-        this.prompt = prompt.clickEvent(event);
     }
 
     public Component component()
